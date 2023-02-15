8d09eb5af43a726bc710ebaf823f882b4f01a4bd
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-02-15 22:07:42 +0500

    Added summa and average arithmetic of elements of array

---
+++ ArrayVolueN/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("\tEnter your value n ");
+double summa=0;
+double everagy=0;
+var n = int.Parse(Console.ReadLine());
+var array=new int[n];
+Console.Write("\tElements of array ");
+var rnd = new Random();
+for (var i = 0; i < n; ++i)
+{
+    array[i] = rnd.Next(-20, 40);
+    Console.Write("  "+array[i]);
+    summa +=array[i] ;
+    everagy = (summa / n);
+}
+Console.Write("\n\tSumma "+summa);
+Console.Write("\n\tEveragy of arithmetic " + everagy);
+
